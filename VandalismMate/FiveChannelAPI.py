b8f5cfa37417cf01c5fc50b9e9fb9abd0543b8aa
seroroon <seron3070@gmail.com>
2022-12-14 16:04:23 +0900

    update

--- VandalismMate/FiveChannelAPI.py
+++ VandalismMate/FiveChannelAPI.py
@@ -9,8 +9,7 @@ import cchardet
 
 class authenticate_5chAPI:
     
-    # 使うapiキーとHMkeyとuserAgentとX-2ch-UAはセット。
-    # https://prokusi.wiki.fc2.com/wiki/%E8%AC%8E%E3%81%AE%E6%96%87%E5%AD%97%E5%88%97
+
     def __init__(    
 
         self,
@@ -37,6 +36,7 @@ class authenticate_5chAPI:
         message = self.AppKey + self.CT
         # python3系統ではhmac.newの引数はbytearrayで変換すること
         HB = hmac.new(bytearray(self.HMKey,"ASCII"), bytearray(message,"ASCII"), hashlib.sha256).hexdigest()
+        # 浪人にログインするならIDとPW入力
         values = {'ID' : '', 'PW' : '', 'KY' : self.AppKey, 'CT' : self.CT, 'HB' : HB }
             
         response = httpx.post(self.App_Url,data=values,headers=self.headers)
@@ -70,7 +70,24 @@ class use_5chAPI(authenticate_5chAPI):
         # 文字コードにISO-8859-1が使われていて文字化けする場合は以下の1行を入れる
         response.encoding = cchardet.detect(response.content)["encoding"]
         return response.text
+    
+    def send_message(
 
+        self,
+        serverName, 
+        boardName, 
+        threadId, 
+        sid
+
+    ):
+
+        message = "/v1/" + serverName + "/" + boardName + "/" + threadId + sid + self.AppKey
+        hobo = hmac.new(bytearray(self.HMKey,"ASCII"), bytearray(message,"ASCII"), hashlib.sha256).hexdigest()
+        url = self.App_Url[:-5] + serverName + "/" + boardName + "/" + threadId
+        values = { 'sid' : sid, 'hobo' : hobo, 'appkey': self.AppKey }
+        response = httpx.post(url,data=values,headers=self.headers,timeout=None)
+        # 文字コードにISO-8859-1が使われていて文字化けする場合は以下の1行を入れる
+        response.encoding = cchardet.detect(response.content)["encoding"]
 
 
 
@@ -83,15 +100,17 @@ def main():
 
     instance = use_5chAPI(
     
-    AppKey = 'a6kwZ1FHfwlxIKJWCq4XQQnUTqiA1P',
-    HMKey = 'ZDzsNQ7PcOOGE2mXo145X6bt39WMz6',
-    CT = '1234567890',
-    AppUrl = 'https://api.5ch.net/v1/auth/',
-    headers =  { 'User-Agent' : 'Monazilla/1.00 JaneStyle/4.23 Windows/10.0.22000', 'X-2ch-UA': 'JaneStyle/4.23'}
+        AppKey = '',
+        HMKey = '',
+        CT = '',
+        AppUrl = 'https://api.5ch.net/v1/auth/',
+        headers =  { 'User-Agent' : 'Monazilla/1.00 JaneStyle/4.23 Windows/10.0.22000', 'X-2ch-UA': 'JaneStyle/4.23'}
     
     )
 
-    dat = instance.getDAT('mi', 'news4vip', '1670646031', sid=instance.sessionIdentifier_Create())
+    #instance.send_message('mi', 'news4vip', '1670714221', sid=instance.sessionIdentifier_Create())
+
+    dat = instance.getDAT('mi', 'news4vip', '1670714221', sid=instance.sessionIdentifier_Create())
     print(dat)
 
 if __name__ == '__main__':
--- VandalismMate/VandalismMate.py
+++ VandalismMate/VandalismMate.py
@@ -5,9 +5,9 @@ def use5chApiTest():
 
     instance = FiveChannelAPI.use_5chAPI(
         
-        AppKey = 'a6kwZ1FHfwlxIKJWCq4XQQnUTqiA1P',
-        HMKey = 'ZDzsNQ7PcOOGE2mXo145X6bt39WMz6',
-        CT = '1234567890',
+        AppKey = '',
+        HMKey = '',
+        CT = '',
         AppUrl = 'https://api.5ch.net/v1/auth/',
         headers =  { 'User-Agent' : 'Monazilla/1.00 JaneStyle/4.23 Windows/10.0.22000', 'X-2ch-UA': 'JaneStyle/4.23'}
         
